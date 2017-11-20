de31a8d79b09aed4f8807d0272f5be36d3396425
kimberlysara <kimberlyshopkins99@gmail.com>
2017-11-19 20:00:18 -0700

    Block Breaker

---
+++ BlockBreaker/Assets/Scripts/Floor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+
+	public LevelManager myLevelManager;
+
+	void Start () {
+		
+	}
+
+	public void OnTriggerEnter2D(Collider2D trigger){
+		//print ("triggered floor");
+		//print (trigger.name);
+		if (trigger.name == "Ball") {
+			myLevelManager.LevelLoad ("GameOver");
+		}
+		}
+		
+	}
+
+		
+
+
---
+++ BlockBreaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public static int brickCount;
+	public static Ball[] balls;
+
+	void Update (){
+	//	public void CheckBrickCount ();
+		//brickCount = FindObjectsOfType<Brick> ().Length;
+	}
+
+	void Start(){
+		brickCount = FindObjectsOfType<Brick> ().Length;
+		print ("Number of Bricks: " + brickCount);
+	}
+
+	public void LevelLoad (string name) {
+		SceneManager.LoadScene (name);
+	}
+		
+	public void ExitGame (){
+		print ("Tried to Exit");
+		Application.Quit ();
+	}
+	public void LoadNextLevel () {
+		print ("loading next level");
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		//SceneManager.LoadScene ("Level03");
+
+	}
+	public void CheckBrickCount () {
+		//brickCount = FindObjectsOfType<Brick> ().Length;
+		print ("Number of Bricks: " + brickCount);
+		if (brickCount <= 0) {
+			LoadNextLevel();
+			//LevelLoad ("Level02");
+			//SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+}
