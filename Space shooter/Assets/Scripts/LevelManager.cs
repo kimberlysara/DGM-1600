5b825634d2f83ddcf45bb761990bd2174aae2aa1
kimberlysara <kimberlyshopkins99@gmail.com>
2017-11-19 20:04:42 -0700

    Space Shooter

---
+++ Space shooter/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public static int meteorCount;
+
+	void Start(){
+		meteorCount = FindObjectsOfType<Meteor> ().Length;
+		print (meteorCount);
+	} 
+
+	public void LevelLoad (string name) {
+		SceneManager.LoadScene (name);
+	}
+		
+	public void ExitGame (){
+		print ("Tried to Exit");
+		Application.Quit ();
+	}
+	public void LoadNextLevel () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	public void CheckMeteorCount (){
+		if (meteorCount <= 0) {
+			LoadNextLevel ();
+		}
+	}
+		}	
+
---
+++ Space shooter/Assets/Scripts/PowerUps.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour {
+
+	public enum Type {fancy ,shield, speed};
+	public Type powerupType;
+	public Sprite[] images;
+
+
+	// Use this for initialization
+	void Start () {
+		switch (powerupType) {
+			case Type.fancy:
+				gameObject.GetComponent<SpriteRenderer> ().sprite = images [0];
+				break;
+		case Type.shield:
+			gameObject.GetComponent<SpriteRenderer> ().sprite = images [1];
+			break;
+
+		case Type.speed:
+			gameObject.GetComponent<SpriteRenderer> ().sprite = images [2];
+			break;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("We got Hit By Something");
+
+	//	if (powerupType == Type.speed) {
+		switch (powerupType) {
+		case Type.speed:
+			other.GetComponent<PlayerController> ().speed *= 2;
+			break;
+		case Type.fancy:
+			
+			break;
+		case Type.shield:
+			
+			break;
+		default:
+			break;
+			
+
+	}
+		Destroy (this.gameObject);
+
+}
+}
