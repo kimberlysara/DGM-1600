00b58d8807cb639a18209b549947d4290683d2e6
kimberlysara <kimberlyshopkins99@gmail.com>
2017-12-10 15:00:10 -0700

    Limited speed

--- Space shooter/Assets/Scripts/PlayerController.cs
+++ Space shooter/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 	//public Vector3 velocity;
 	public float shipSpeed;
-	public float speed = 200.0f;
-	public const float startSpeed = 200.0f;
+	public float speed = 150.0f;
+	public const float startSpeed = 150.0f;
 	public float speedTimer;
+	public float laserTimer;
+	public float fireRate;
 
 	public ParticleSystem particles;
 
@@ -16,7 +18,7 @@ public class PlayerController : MonoBehaviour {
 	public float shotForce;
 
 	void Start () {
-
+		fireRate = 0.8f;
 		print (GetComponent<Health>().GetHealth() ); 
 	}
 
@@ -24,6 +26,7 @@ public class PlayerController : MonoBehaviour {
 		//print (speed);
 	   speed *= 2;
 		speedTimer = 3.0f;
+
 	}
 
 
@@ -35,6 +38,11 @@ public class PlayerController : MonoBehaviour {
 				speed = startSpeed;
 			}
 		}
+		if (laserTimer > 0) {
+			laserTimer -= Time.deltaTime;
+
+		}
+	
 		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
 			transform.position += move * speed * Time.deltaTime;
 		//check for button push
@@ -54,10 +62,12 @@ public class PlayerController : MonoBehaviour {
 			particles.Emit (1);
 		}
 		if (Input.GetButtonUp ("Fire1")) {
+			if (laserTimer <= 0){
 		GameObject shot = Instantiate (projectile, shotPos.position, shotPos.rotation) as GameObject; 
 			shot.GetComponent<Rigidbody2D> ().AddForce (shotPos.up * shotForce);
+				laserTimer = fireRate;
 				//shotPos.AddForce (shotPos.forward * shotForce)
-
+			}
 		}
 		//switch (health) {
 		//case 1:
